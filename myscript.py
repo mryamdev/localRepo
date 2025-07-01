a511fa26eb83ef5d4a3c81a23772ba4e1388deb3
mryamdev <toobamaryam1020@gmail.com>
2025-07-01 01:34:07 -0700

    Add new paragraph

--- myscript.py
+++ myscript.py
@@ -1,1 +1,43 @@
 print("maryam")
+name = "maryam"
+age = 20
+age2= age
+price= 23.5
+print("my name is:",name)
+print("my age is:" ,age)
+print("my price is:",price)
+print(age2)
+# to print the type variable
+print(type(name))
+print(type(age))
+print(type(price))
+# string print to the comma
+name1="TM" 
+name2='TM'
+name3= '''TM'''
+print(name1)
+print(name2)
+print(name3)
+# sum of a and b
+a=3
+b=5
+sum=a+b
+print(sum)
+# diff of a and b
+dif=a-b
+print(dif)
+# multi of a and b
+multi=a*b
+print(multi)
+# modulue of a and b
+module=a%b
+print(module)
+# cnditional statement
+name=input("name:")
+age=int(input("age:"))
+if(age<=18):
+    print("eligible")
+elif(age==20):
+    print("not eligible")
+else:
+    print("invalid value")
